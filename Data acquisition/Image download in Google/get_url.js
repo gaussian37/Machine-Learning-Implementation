15ef524d0cf72c40ae63294ac61d6f842031dc95
gaussian37 <gaussian37@hyundai.com>
2017-12-13 21:39:52 +0900

    rev.0

---
+++ Data acquisition/Image download in Google/get_url.js
@@ -0,0 +1,14 @@
+// pull down jquery into the JavaScript console
+var script = document.createElement('script');
+script.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js";
+document.getElementsByTagName('head')[0].appendChild(script);
+
+// pull down jquery into the JavaScript console
+var script = document.createElement('script');
+script.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js";
+document.getElementsByTagName('head')[0].appendChild(script);
+
+// pull down jquery into the JavaScript console
+var script = document.createElement('script');
+script.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js";
+document.getElementsByTagName('head')[0].appendChild(script);
